723728aafd57e92d108169b58955923e3eff87e4
peasantdev <peasantwebdev@outlook.com>
2023-05-03 16:05:52 -0700

    Add project files.

---
+++ ConsoleApp8/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp8;
+
+
+
+
+//Class Employee
+class Employee
+    {
+    public int e;
+        public Employee() 
+        {
+            e = 0;
+        }
+        public Employee(int id)
+        {
+            e = id;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    
+
+    //Overloading the equals operator as you have to overload them in pairs.
+     public static bool operator == (Employee id,
+        Employee other)
+    {
+    if (id.e == other.e)
+        return true;
+    else
+        return false;
+    }
+    //Overloading not equals operator as you have to overload them in pairs.
+    public static bool operator != (Employee id, Employee other)
+    {
+    if (id.e != other.e)
+        return true;
+    else
+        return false;
+    }
+    //The two override statements are needed or will throw errors when overloading the equals operator.
+    public override bool Equals(object o)
+    {
+    if (!(o is Employee))
+        return false;
+    return this == (Employee)o;
+    }
+    
+    public override int GetHashCode()
+    {
+    return 0;
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiated employee ids with input.
+            Employee id1 = new Employee(9);
+            Employee id2 = new Employee(10);
+
+            if (id1 == id2)
+                Console.WriteLine("Equal");
+            else Console.WriteLine("Not Equal");
+            Console.ReadLine();
+        }
+    }
+}
